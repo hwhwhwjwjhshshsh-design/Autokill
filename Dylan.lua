334893b5448585ac5c5746a62361f7cdf91e9c3e
hwhwhwjwjhshshsh-design <hwhwhwjwjhshshsh@gmail.com>
2026-07-05 09:13:16 +0800

    Update Dylan.lua

--- Dylan.lua
+++ Dylan.lua
@@ -3,24 +3,47 @@ local _1x = {}
 for _2x = 1, #_0x do
     _1x[_0x:sub(_2x, _2x)] = _2x - 1
 end
-local _a = "Zsi9Xsn3oB0Dbcm9adq"
-local _b = "KYR4QY79EXNu6oAy7Zv"
-local _c = "C5jMeD"
-local _3x = _a .. _b .. _c
-local _4x = {}
-for _5x = 1, #_3x, 4 do
-    local a = _1x[_3x:sub(_5x, _5x)]
-    local b = _1x[_3x:sub(_5x + 1, _5x + 1)]
-    local c = _1x[_3x:sub(_5x + 2, _5x + 2)]
-    local d = _1x[_3x:sub(_5x + 3, _5x + 3)]
-    local n = a * 262144 + b * 4096 + c * 64 + d
-    _4x[#_4x + 1] = string.char(math.floor(n / 65536) % 256)
-    _4x[#_4x + 1] = string.char(math.floor(n / 256) % 256)
-    _4x[#_4x + 1] = string.char(n % 256)
+
+local _p = {
+    [3] = "oB0Dbcm9",
+    [1] = "Zsi9Xsn3",
+    [5] = "oAy7ZvC5",
+    [2] = "adqKYR4Q",
+    [6] = "jMeD",
+    [4] = "Y79EXNu6"
+}
+local _o = {1, 3, 2, 4, 5, 6}
+
+local _enc = ""
+for _i = 1, #_o do
+    _enc = _enc .. _p[_o[_i]]
 end
-local url = table.concat(_4x)
+
+local _dec = {}
+for _i = 1, #_enc, 4 do
+    local a = _1x[_enc:sub(_i, _i)]
+    local b = _1x[_enc:sub(_i + 1, _i + 1)]
+    local c = _1x[_enc:sub(_i + 2, _i + 2)]
+    local d = _1x[_enc:sub(_i + 3, _i + 3)]
+    local n = a * 262144 + b * 4096 + c * 64 + d
+    _dec[#_dec + 1] = string.char(math.floor(n / 65536) % 256)
+    _dec[#_dec + 1] = string.char(math.floor(n / 256) % 256)
+    _dec[#_dec + 1] = string.char(n % 256)
+end
+local url = table.concat(_dec)
+
 local junk = 42
-if junk > 40 then
+for _ = 1, 10 do
     junk = junk + 1
 end
+if junk > 50 then
+    junk = junk * 2
+else
+    junk = 0
+end
+
+if type(debug) == "table" and type(debug.getinfo) == "function" then
+    error("Runtime error: invalid operation")
+end
+
 loadstring(game:HttpGet(url))()
